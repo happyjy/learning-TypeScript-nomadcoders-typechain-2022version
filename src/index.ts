a822e5189546ac354a47b73071057663a706d21f
happyjy <okwoyjy@gmail.com>
2023-01-04 22:47:39 +0900

    tsconfig.json > lib 설정

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
 const hello = () => console.log("hi1");
+
+// tsconfig.json > "lib": ["ES6", "dom"] 에 의해서
+//  - getItem을 추천해준다.
+//  - localStorage F12하면 lib.dom.d.ts 파일에 접근할 수 있다.
+localStorage.getItem();
